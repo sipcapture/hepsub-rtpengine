75cdcd350b9e64584ff97ebec7489cfd5bf67856
Callum <callum.guy@x-on.co.uk>
2022-12-21 22:44:27 +0000

    improve HEP protocol support

--- rtpengine.js
+++ rtpengine.js
@@ -5,187 +5,270 @@
  */
 
 try {
-  var config = require('./config.js');
-} catch(e) { console.log('Missing config!',e); process.exit(1); }
+    var {
+        backend: homerApiUri,
+        debug: debug,
+        rtpengine: {
+            download: configDownloadEnabled,
+            path: pathRecording,
+        },
+        service: settings,
+        token: homerAuthToken
+    } = require('./config.js');
+} catch (e) {
+    console.log('Could not find config.js', e);
+    process.exit(1);
+}
 
-var LRU = require('lru');
-// PCAP CACHE: MAX NUMBER OF PCAP LINKS
-var cache = new LRU({ max: 10000 });
-// TOKEN CACHE: SET EXPIRATION FOR DOWNLOAD LINKS
-var tokens = new LRU({ max: 100, maxAge: 300000 });
+const LRU = require('lru');
+// PCAP CACHE: MAX NUMBER OF PCAP LINKS todo Move to config
+const cache = new LRU({max: 10000});
+// TOKEN CACHE: SET EXPIRATION FOR DOWNLOAD LINKS todo Move to config
+const tokens = new LRU({max: 100, maxAge: 300000});
 
 const chokidar = require('chokidar');
 const fs = require('fs');
 
-var express = require('express');
+const express = require('express');
 const app = express();
-const util = require('util')
 const request = require('request');
-var bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
-var port = config.service.port;
-var debug = config.debug || false;
+const port = settings.port;
+const debugActive = debug || false;
 
-const requestPromise = util.promisify(request);
+const uuidV1 = require('uuid/v1');
+let agentUUID = uuidV1();
+const ttl = settings.ttl;
 
 // INDEXING Backend for RTPEngine Meta files
-const watcher = chokidar.watch(config.rtpengine.path || '/recording', {ignored: /^\./, persistent: true });
+const watcher = chokidar.watch(pathRecording, {ignored: /^\./, persistent: true});
 watcher
-    .on('error', function(error) {console.error('Error happened', error);})
-    .on('add', function(path) {
-		if (debug) console.log('File', path, 'has been added');
-		if (path.endsWith('.pcap')){
-			var index = {};
-			index.pcap = path;
-			try {
-				index.cid = path.match(/\/([^\/]+)\/?\.pcap$/)[1];
-				var lastIndex = index.cid.lastIndexOf('-');
-				index.cid = decodeURIComponent(index.cid.substring(0,lastIndex));
-			} catch(e) { console.log(e); }
-			var stats = fs.statSync(index.pcap);
-			var datenow = stats.mtime ? new Date(stats.mtime).getTime() : new Date().getTime();
-			index.t_sec = Math.floor( datenow / 1000);
-			index.u_sec = ( datenow - (index.t_sec*1000))*1000;
-			if (debug) console.log('PCAP Hit!', index);
-			cache.set(index.cid, JSON.stringify(index));
-		} else {
-			if (debug) console.log('unmatched type/path', path);
-		}
+    .on('error', function (error) {
+        console.error('directory watcher error:', error);
     })
-    // .on('change', function(path) {console.log('File', path, 'has been changed'); })
-    .on('unlink', function(path) { if (debug) console.log('File', path, 'has been removed. Indexing!');
-	   if(path.includes('rtpengine-meta')){
-                	var index = {}; // Recording Object
-			index.meta = path;
-                	index.meta = index.meta.replace(/tmp\//i, 'metadata/');
-                	index.meta = index.meta.replace(/\.tmp/i, '.txt');
-                	index.pcap = path;
-                	index.pcap = index.pcap.replace(/tmp\/rtpengine-meta-/i, 'pcaps/');
-                	index.pcap = index.pcap.replace(/\.tmp/i, '.pcap');
-			try {
-				index.cid = index.pcap.match(/\/([^\/]+)\/?\.pcap$/)[1];
-				var lastIndex = index.cid.lastIndexOf('-');
-				index.cid = decodeURIComponent(index.cid.substring(0,lastIndex));
-			} catch(e) { index.cid = false; console.log(e); }
-
-			var stats = fs.statSync(index.pcap);
-			var datenow = stats.mtime ? new Date(stats.mtime).getTime() : new Date().getTime();
-			index.t_sec = Math.floor( datenow / 1000);
-			index.u_sec = ( datenow - (index.t_sec*1000))*1000;
-			if (debug) console.log('Meta Hit!', index);
-			cache.set(index.cid, JSON.stringify(index));
-	   }
+    .on('add', function (path) {
+        if (path.endsWith('.pcap')) {
+            if (debugActive) {
+                console.log('attempting to add', path, 'to index');
+            }
+            let index = {};
+            index.pcap = path;
+            try {
+                index.cid = extractCallIDFromPCAP(index.pcap)
+                // Get the file modification time of a PCAP into our file index
+                let stats = fs.statSync(index.pcap);
+                let dateNow = stats.mtime ? new Date(stats.mtime).getTime() : new Date().getTime();
+                index.t_sec = Math.floor(dateNow / 1000);
+                index.u_sec = (dateNow - (index.t_sec * 1000)) * 1000;
+                if (debugActive) {
+                    console.log('added call to index:', index);
+                } else {
+                    console.log('added call to index:', index.cid);
+                }
+                cache.set(index.cid, JSON.stringify(index));
+            } catch (e) {
+                console.log('failed to add', path, 'to index:', e);
+            }
+        } else {
+            if (debugActive) {
+                console.log('ignoring unmatched file type:', path);
+            }
+        }
+    })
+    .on('unlink', function (path) {
+        // when files are removed from the filesystem we should also remove from our index/cache
+        if (path.endsWith('.pcap')) {
+            try {
+                let cid = extractCallIDFromPCAP(path)
+                cache.remove(cid);
+                console.log('removed call from index due to file removal:', cid);
+            } catch (e) {
+                console.log('failed to remove file', path, 'from index. error:', e);
+            }
+        }
     });
 
 // API SETTINGS
-app.all('*', function(req, res, next) {
-   res.header("Access-Control-Allow-Origin", "*");
-   res.header("Access-Control-Allow-Headers", "X-Requested-With");
-   next();
+app.all('*', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
 });
 
-
-var processDownload = function(req,res){
-    try {
-	var data = req.body;
-	if (debug) console.log('NEW DOWNLOAD REQ', JSON.stringify(data), req.params.file, req.url);
-	// SECURE: backend should provide a valid token to proceed
-  	if(data.__hep__ && data.__hep__.token && data.pcap){
-		// validate ephemeral token
-		var token = tokens.get(data.__hep__.token);
-		if (!token) { res.sendStatus(401); return; }
-		var fullPath = data.pcap;
-		var stats = fs.statSync(fullPath);
-		if (stats) { res.download(fullPath, req.params.file); }
-	} else { res.sendStatus(404); }
-    } catch(e) { console.error(e); res.sendStatus(500); }
+// RTPEngine adds a suffix to call ID in the PCAP file name, extract the CID
+const extractCallIDFromPCAP = function (path) {
+    let cid = path.match(/\/([^\/]+)\/?\.pcap$/)[1];
+    cid = decodeURIComponent(cid.substring(0, cid.lastIndexOf('-')));
+    return cid;
 }
 
-// HEP Post Paths
-app.post('/get/:id', async function (req, res) {
-  try {
-	  // Download Request
-	  if (req.params.id == "download"){
-		if (!config.rtpengine.download){ res.sendStatus(500); return; }
-		processDownload(req,res);
-		return;
-	  }
-	  // HEPsub request
-	  var apiresponse = {};
-	  var data = req.body;
-	  if (debug) console.log('NEW API POST REQ', JSON.stringify(data));
-	  if (data.data && data.data.sid) {
-	    await Promise.all(data.data.sid.map(async (cid) => {
-	    	var cached = cache.get(cid);
-	    	if (cached) {
-                        if (debug) console.log('Found Index in Cache',cid,cached);
-			apiresponse[cid] = JSON.parse(cached) || cached;
-		    	apiresponse[cid]['__hep__'] = {};
-		    	apiresponse[cid]['__hep__'].token = uuidv1();
-		    	apiresponse[cid]['__hep__'].type = "download";
-		    	apiresponse[cid]['__hep__'].uuid = uuid;
-		        tokens.set(apiresponse[cid]['__hep__'].token, new Date().toISOString() );
-		}
-	    }));
-
-            if (debug) console.log('API RESPONSE',apiresponse);
-	    res.send(apiresponse)
-
-	  } else { res.sendStatus(500); }
-  } catch(e) { console.error(e) }
-})
-
-app.listen(port, () => console.log('API Server started',port))
-
-// HEP PUBSUB Hooks
-var api = config.backend;
-const uuidv1 = require('uuid/v1');
-var uuid = uuidv1();
-var ttl = config.service.ttl;
-var token = config.token;
-
-var publish = function(){
-  try {
-    var settings = config.service;
-    settings.uuid = uuid;
-    const data = JSON.stringify(settings)
-    const options = {
-        url: api,
-        method: 'POST',
-        json: settings,
-        headers: {
-          'Auth-Token': token
+// Homer UI provides a download link when the PCAP lookup was successful, process these download requests
+const processDownload = function (req, res) {
+    try {
+        let data = req.body;
+        if (debugActive) {
+            console.log('processing download request', JSON.stringify(data), req.url);
         }
-    }
-
-    if (debug) console.log("Body:", JSON.stringify(options));
-
-    request(options, function (error, response, body) {
-        if (!error && (response.statusCode == 200 || response.statusCode == 201)) {
-            if (debug) {
-                console.log("RESPONSE API:", body) // Print the shortened url.
+        // SECURE: backend should provide a valid token to proceed
+        if (data.data.__hep__ && data.data.__hep__[0].token) {
+            // valid tokens live in cache
+            let pcapPath = tokens.get(data.data.__hep__[0].token);
+            if (!pcapPath) {
+                console.log('token validation failed');
+                res.sendStatus(401);
+                return;
+            }
+            // check file exists and return it
+            let stats = fs.statSync(pcapPath);
+            if (stats) {
+                res.download(pcapPath);
             }
         } else {
-            if (debug) {
-                if(body && body.message) console.log('REGISTER API ERROR: ', body.message);
-                else console.log('REGISTER UNKNOWN ERROR: ', error);
+            res.sendStatus(404);
+        }
+    } catch (e) {
+        console.error(e);
+        res.sendStatus(500);
+    }
+}
+
+// API metadata & PCAP retrieval - Homer will call this with a call ID
+app.post('/get/:action', async function (req, res) {
+    // Example request:
+    //    {
+    //      data: {
+    //        sid: [ '101394311-3879497848-1392324686@example.com' ],
+    //        source_ip: [
+    //          '1.2.3.4',
+    //          '2.2.2.2',
+    //        ]
+    //        "__hep__": [{    <<< present for download requests
+    //            "token": "6838ee50-817d-11ed-b83f-57bc29d00545", <<< auth token
+    //            "type": "download",
+    //            "uuid": "01a8d7a0-812c-11ed-b83f-57bc29d00545"   <<< hepsub agent id
+    //        }],
+    //      },
+    //      fromts: 1670508449000,
+    //      tots: 1670509549000
+    //    }
+    try {
+        // valid actions = download / pcap
+        switch (req.params.action) {
+            case 'download': {
+                if (!configDownloadEnabled) {
+                    res.sendStatus(500);
+                } else {
+                    processDownload(req, res);
+                }
+                return;
+            }
+            case 'pcap': {
+                // check cache for pcap on this host
+                let data = req.body;
+                if (debugActive) {
+                    console.log('incoming pcap lookup request', JSON.stringify(data));
+                }
+                if (!data.data || !data.data.sid) {
+                    res.sendStatus(500);
+                    return;
+                }
+
+                let found = false;
+                let apiResponse = {};
+
+                await Promise.all(data.data.sid.map(async (cid) => {
+                    let cached = cache.get(cid);
+                    if (cached) {
+                        found = true;
+                        if (debugActive) {
+                            console.log('found Index in Cache', cid, cached);
+                        }
+                        let cacheData = JSON.parse(cached)
+                        if (!cacheData || !cacheData.pcap) {
+                            console.log('failed to load metadata from cache, even though object was matched. cache data:', cacheData);
+                            return;
+                        }
+                        apiResponse[cid] = cacheData;
+                        apiResponse[cid]['__hep__'] = {};
+                        apiResponse[cid]['__hep__'].token = uuidV1(); // generate new access token for download authentication
+                        apiResponse[cid]['__hep__'].type = "download";
+                        apiResponse[cid]['__hep__'].uuid = agentUUID;
+                        // each time we generate a HEP object with a download option, cache the UUID as an access token for the download
+                        if (debugActive) {
+                            console.log('storing pcap path', cacheData.pcap, 'against token', apiResponse[cid]['__hep__'].token);
+                        }
+                        tokens.set(apiResponse[cid]['__hep__'].token, cacheData.pcap);
+                    }
+                }));
+
+                if (found === false) {
+                    console.log('404 pcap not found.');
+                    res.sendStatus(404);
+                    return;
+                }
+
+                console.log('200 pcap found.', apiResponse);
+                res.send(apiResponse)
+
+                return;
+            }
+            default: {
+                console.log('404 method not allowed.');
+                res.sendStatus(405);
+                return;
             }
         }
-    });
+    } catch (e) {
+        console.error('pcap lookup failed with error: ', e);
+    }
+})
 
-  } catch(e) { console.error(e) }
+app.listen(port, () => console.log('API Server started', port))
+
+// Call the Homer API to register this application for HEP PUB-SUB hooks
+const refreshSubscription = function () {
+    try {
+        settings.uuid = agentUUID;
+
+        let requestOptions = {
+            url: homerApiUri,
+            method: 'POST',
+            json: settings,
+            headers: {
+                'Auth-Token': homerAuthToken
+            }
+        }
+
+        if (debugActive) {
+            console.log("body:", JSON.stringify(requestOptions));
+        }
+
+        request(requestOptions, function (error, response, body) {
+            if (!error && (response.statusCode === 200 || response.statusCode === 201)) {
+                if (debugActive) {
+                    console.log("hepsub session refreshed. response:", body)
+                }
+            } else {
+                if (debugActive) {
+                    if (body && body.message) {
+                        console.log('hepsub session refresh. error: ', error, 'body:', body.message);
+                    } else {
+                        console.log('homer subscription error: ', error);
+                    }
+                }
+            }
+        });
+    } catch (e) {
+        console.error('homer subscription failed with error: ', e);
+    }
 }
 
-/* REGISTER SERVICE w/ TTL REFRESH */
+// Register with Homer on application start. optional: reregister after 90% session TTL
+refreshSubscription();
 if (ttl) {
-	publish();
-	/* REGISTER LOOP */
-	setInterval(function() {
-	   publish()
-	}, (.9 * ttl)*1000 );
+    setInterval(function () {
+        refreshSubscription()
+    }, (.9 * ttl) * 1000);
 }
-
-/* END */
-
-
